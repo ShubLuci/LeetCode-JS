a663c91ac197362aa794e512117ce6cb4470c44b
Shubham Yadav <cshubyadav@gmail.com>
2025-01-22 11:52:39 +0530

    LeetCode #22-01-2025

---
+++ 009_Palindrome_Number#2.js
@@ -0,0 +1,56 @@
+/*
+
+Link: https://leetcode.com/problems/palindrome-number/description/
+
+Title: Palindrome Number (Try Solving without converting it to string)
+
+Question: 
+
+Given an integer x, return true if x is a 
+palindrome
+, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+
+*/
+
+/**
+ * @param {number} x
+ * @return {boolean}
+*/
+
+var isPalindrome = function(x) {
+    let tempNum = x;
+    let finalNum = 0;
+    while(tempNum>0) {
+        finalNum = (finalNum*10) + (tempNum%10);
+        tempNum = Math.floor(tempNum/10);
+    }
+    if(finalNum === x)
+        return true
+    else
+        return false
+};
+ 
+ console.log(isPalindrome(121))
