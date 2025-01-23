c13fc81aa24baaf70fa77abb8ec8d60c5f3f1e0c
Shubham Yadav <cshubyadav@gmail.com>
2025-01-23 16:08:38 +0530

    Formatting Fix

--- 013_Roman_To_Integer.js
+++ 013_Roman_To_Integer.js
@@ -27,17 +27,18 @@ Given a roman numeral, convert it to an integer.
  
 
 Example 1:
-
 Input: s = "III"
 Output: 3
 Explanation: III = 3.
-Example 2:
 
+
+Example 2:
 Input: s = "LVIII"
 Output: 58
 Explanation: L = 50, V= 5, III = 3.
-Example 3:
 
+
+Example 3:
 Input: s = "MCMXCIV"
 Output: 1994
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
